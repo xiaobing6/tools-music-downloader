df84c35ddbecca2dc22a16fe652169842a983567
xiaobing6 <xiaobing6@users.noreply.github.com>
2026-07-06 22:39:21 +0800

    统一开发语言

--- music_downloader/gui/frontend/svelte.config.ts
+++ music_downloader/gui/frontend/svelte.config.ts
@@ -1,6 +1,7 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import type { SvelteConfig } from "@sveltejs/vite-plugin-svelte";
 
-const config = {
+const config: SvelteConfig = {
   preprocess: vitePreprocess()
 };
 
